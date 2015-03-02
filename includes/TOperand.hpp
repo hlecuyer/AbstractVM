0cfd39b69455385578a15b0c06962d744c675ad1
johnduro <martin.leroy6@gmail.com>
2015-03-02 20:21:19 +0100

    rien

--- includes/TOperand.hpp
+++ includes/TOperand.hpp
@@ -17,10 +17,6 @@ enum operationType {
 	divOp,
 	modOp
 };
-// #include "VirtualMachine.hpp"
-
-// class VirtualMachine;
-// class OperandFactory;
 
 template <typename T>
 class TOperand : public IOperand
@@ -32,27 +28,13 @@ private:
 	std::string			_typeStr;
 	OperandFactory		_operandFactory;
 
-	// template <typename Q>
-	// struct operatorWrapper {
-	// 	typedef Q (*operatorFunctionPtr)(Q a, Q b) const;
-	// };
-
-	// static const operatorWrapper::operatorFunctionPtr 					_operators[];
-
 	TOperand();
 	void		_findType( void );
-	// IOperand const *	_addOperand( IOperand const & rhs ) const;
-	template <typename C>
-	IOperand const *	_addOperand( TOperand<C> const * rhs ) const ;
+
+	IOperand const *	_castOperand( IOperand const & rhs, operationType operation) const ;
 
 	template <typename C>
-	IOperand const *	_subOperand( TOperand<C> const * rhs ) const ;
-
-	template <typename C>
-	IOperand const *	_mulOperand( TOperand<C> const * rhs ) const ;
-
-	template <typename C>
-	IOperand const *	_genericOperand( TOperand<C> const * rhs, operationType operation ) const ;
+	IOperand const *	_genericOperation( TOperand<C> const * rhs, operationType operation ) const ;
 
 	template <typename C>
 	C					_operatorsSwitch(C a, C b, operationType operation) const ;
@@ -154,7 +136,7 @@ Q TOperand<T>::_modOp (Q a, Q b) const
 
 template <typename T>
 template <typename C>
-IOperand const *	TOperand<T>::_genericOperand( TOperand<C> const * rhs, operationType operation ) const
+IOperand const *	TOperand<T>::_genericOperation( TOperand<C> const * rhs, operationType operation ) const
 {
 	std::string		ret;
 	eOperandType	type;
@@ -164,7 +146,7 @@ IOperand const *	TOperand<T>::_genericOperand( TOperand<C> const * rhs, operatio
 		C		newVal;
 		C		result;
 
-		newVal = boost::numeric_cast<C>(this->_value);//VERIFIER EXCEPTION
+		newVal = boost::numeric_cast<C>(this->_value);
 		result = this->_operatorsSwitch<C> (newVal, rhs->getValue(), operation);
 		ret = boost::lexical_cast<std::string>(result);
 		type = rhs->getType();
@@ -174,7 +156,7 @@ IOperand const *	TOperand<T>::_genericOperand( TOperand<C> const * rhs, operatio
 		T		newVal;
 		T		result;
 
-		newVal = boost::numeric_cast<T>(rhs->getValue());//VERIFIER EXCEPTION
+		newVal = boost::numeric_cast<T>(rhs->getValue());
 		result = this->_operatorsSwitch<T> (this->_value, newVal, operation);
 		ret = boost::lexical_cast<std::string>(result);
 		type = this->getType();
@@ -182,120 +164,28 @@ IOperand const *	TOperand<T>::_genericOperand( TOperand<C> const * rhs, operatio
 	return (this->_operandFactory.createOperand(type, ret));
 }
 
-// template <typename T>
-// template <typename C>
-// IOperand const *	TOperand<T>::_addOperand( TOperand<C> const * rhs ) const
-// {
-// 	std::string		ret;
-// 	eOperandType	type;
-
-// 	if (this->getPrecision() < rhs->getPrecision())
-// 	{
-// 		C		newVal;
-// 		C		result;
-
-// 		newVal = boost::numeric_cast<C>(this->_value);//VERIFIER EXCEPTION
-// 		result = newVal + rhs->getValue();
-// 		ret = boost::lexical_cast<std::string>(result);
-// 		type = rhs->getType();
-// 	}
-// 	else
-// 	{
-// 		T		newVal;
-// 		T		result;
-
-// 		newVal = boost::numeric_cast<T>(rhs->getValue());//VERIFIER EXCEPTION
-// 		result = this->_value + newVal;
-// 		ret = boost::lexical_cast<std::string>(result);
-// 		type = this->getType();
-// 	}
-// 	return (this->_operandFactory.createOperand(type, ret));
-// }
-
-// template <typename T>
-// template <typename C>
-// IOperand const *	TOperand<T>::_subOperand( TOperand<C> const * rhs ) const
-// {
-// 	std::string		ret;
-// 	eOperandType	type;
-
-// 	if (this->getPrecision() < rhs->getPrecision())
-// 	{
-// 		C		newVal;
-// 		C		result;
-
-// 		newVal = boost::numeric_cast<C>(this->_value);//VERIFIER EXCEPTION
-// 		result = newVal - rhs->getValue();
-// 		ret = boost::lexical_cast<std::string>(result);
-// 		type = rhs->getType();
-// 	}
-// 	else
-// 	{
-// 		T		newVal;
-// 		T		result;
-
-// 		newVal = boost::numeric_cast<T>(rhs->getValue());//VERIFIER EXCEPTION
-// 		result = this->_value - newVal;
-// 		ret = boost::lexical_cast<std::string>(result);
-// 		type = this->getType();
-// 	}
-// 	return (this->_operandFactory.createOperand(type, ret));
-// }
-
-// template <typename T>
-// template <typename C>
-// IOperand const *	TOperand<T>::_mulOperand( TOperand<C> const * rhs ) const
-// {
-// 	std::string		ret;
-// 	eOperandType	type;
-
-// 	if (this->getPrecision() < rhs->getPrecision())
-// 	{
-// 		C		newVal;
-// 		C		result;
-
-// 		newVal = boost::numeric_cast<C>(this->_value);//VERIFIER EXCEPTION
-// 		result = newVal - rhs->getValue();
-// 		ret = boost::lexical_cast<std::string>(result);
-// 		type = rhs->getType();
-// 	}
-// 	else
-// 	{
-// 		T		newVal;
-// 		T		result;
-
-// 		newVal = boost::numeric_cast<T>(rhs->getValue());//VERIFIER EXCEPTION
-// 		result = this->_value - newVal;
-// 		ret = boost::lexical_cast<std::string>(result);
-// 		type = this->getType();
-// 	}
-// 	return (this->_operandFactory.createOperand(type, ret));
-// }
-
 template <typename T>
 TOperand<T>::TOperand(std::string value) : _strValue(value)
 {
-	// this->_type = this->_findType();
 	this->_findType();
-	// this->_type = TOperand::_findType();
-	// this->_type = TOperand::_findType<T>();
+
 	try
 	{
 		this->_value = boost::numeric_cast<T>(boost::lexical_cast<double>(value));
 	}
 	catch ( boost::numeric::positive_overflow & e)
 	{
-		std::cout << "AbstractVM: " << this->_typeStr << ": " << e.what() << std::endl;//MODFIER
+		std::cout << "AbstractVM: " << this->_typeStr << ": " << e.what() << std::endl;
 		std::exit(-1);
 	}
 	catch ( boost::numeric::negative_overflow & e)
 	{
-		std::cout << "AbstractVM: " << this->_typeStr << ": " << e.what() << std::endl;//MODFIER
+		std::cout << "AbstractVM: " << this->_typeStr << ": " << e.what() << std::endl;
 		std::exit(-1);
 	}
 	catch ( boost::bad_lexical_cast const & e)
 	{
-		std::cout << "AbstractVM: " << this->_typeStr << ": " << e.what() <<  std::endl;//MODFIER
+		std::cout << "AbstractVM: " << this->_typeStr << ": " << e.what() <<  std::endl;
 		std::exit(-1);
 	}
 }
@@ -346,104 +236,54 @@ std::string const & TOperand<T>::toString( void ) const
 	return (this->_strValue);
 }
 
+
 template <typename T>
-IOperand const * TOperand<T>::operator+( IOperand const & rhs ) const
+IOperand const * TOperand<T>::_castOperand( IOperand const & rhs, operationType operation) const
 {
 	switch (rhs.getType())
 	{
 	case eOperandType::int8 :
-		return (this->_genericOperand<int8_t>(reinterpret_cast< TOperand<int8_t> const * >(&rhs), operationType::addOp ));
+		return (this->_genericOperation<int8_t>(reinterpret_cast< TOperand<int8_t> const * >(&rhs), operation ));
 		break ;
 	case eOperandType::int16 :
-		return (this->_genericOperand<int16_t>( reinterpret_cast< TOperand<int16_t> const * >(&rhs), operationType::addOp ));
+		return (this->_genericOperation<int16_t>( reinterpret_cast< TOperand<int16_t> const * >(&rhs), operation ));
 		break ;
 	case eOperandType::int32 :
-		return (this->_genericOperand<int32_t>(  reinterpret_cast< TOperand<int32_t> const * >(&rhs), operationType::addOp ));
+		return (this->_genericOperation<int32_t>(  reinterpret_cast< TOperand<int32_t> const * >(&rhs), operation ));
 		break ;
 	case eOperandType::floatt :
-		return (this->_genericOperand<float>(  reinterpret_cast< TOperand<float> const * >(&rhs), operationType::addOp ));
+		return (this->_genericOperation<float>(  reinterpret_cast< TOperand<float> const * >(&rhs), operation ));
 		break ;
 	case eOperandType::doublee :
-		return (this->_genericOperand<double>(  reinterpret_cast< TOperand<double> const * >(&rhs), operationType::addOp ));
+		return (this->_genericOperation<double>(  reinterpret_cast< TOperand<double> const * >(&rhs), operation ));
 		break ;
 	}
 
 	return this;
+}
+
+template <typename T>
+IOperand const * TOperand<T>::operator+( IOperand const & rhs ) const
+{
+	return (this->_castOperand(rhs, operationType::addOp));
 }
 
 template <typename T>
 IOperand const * TOperand<T>::operator-( IOperand const & rhs ) const
 {
-	switch (rhs.getType())
-	{
-	case eOperandType::int8 :
-		return (this->_genericOperand<int8_t>(reinterpret_cast< TOperand<int8_t> const * >(&rhs), operationType::subOp ));
-		break ;
-	case eOperandType::int16 :
-		return (this->_genericOperand<int16_t>( reinterpret_cast< TOperand<int16_t> const * >(&rhs), operationType::subOp ));
-		break ;
-	case eOperandType::int32 :
-		return (this->_genericOperand<int32_t>(  reinterpret_cast< TOperand<int32_t> const * >(&rhs), operationType::subOp ));
-		break ;
-	case eOperandType::floatt :
-		return (this->_genericOperand<float>(  reinterpret_cast< TOperand<float> const * >(&rhs), operationType::subOp ));
-		break ;
-	case eOperandType::doublee :
-		return (this->_genericOperand<double>(  reinterpret_cast< TOperand<double> const * >(&rhs), operationType::subOp ));
-		break ;
-	}
-
-	return this;
+	return (this->_castOperand(rhs, operationType::subOp));
 }
 
 template <typename T>
 IOperand const * TOperand<T>::operator*( IOperand const & rhs ) const
 {
-	switch (rhs.getType())
-	{
-	case eOperandType::int8 :
-		return (this->_genericOperand<int8_t>(reinterpret_cast< TOperand<int8_t> const * >(&rhs), operationType::mulOp ));
-		break ;
-	case eOperandType::int16 :
-		return (this->_genericOperand<int16_t>( reinterpret_cast< TOperand<int16_t> const * >(&rhs), operationType::mulOp ));
-		break ;
-	case eOperandType::int32 :
-		return (this->_genericOperand<int32_t>(  reinterpret_cast< TOperand<int32_t> const * >(&rhs), operationType::mulOp ));
-		break ;
-	case eOperandType::floatt :
-		return (this->_genericOperand<float>(  reinterpret_cast< TOperand<float> const * >(&rhs), operationType::mulOp ));
-		break ;
-	case eOperandType::doublee :
-		return (this->_genericOperand<double>(  reinterpret_cast< TOperand<double> const * >(&rhs), operationType::mulOp ));
-		break ;
-	}
-
-	return this;
+	return (this->_castOperand(rhs, operationType::mulOp));
 }
 
 template <typename T>
 IOperand const * TOperand<T>::operator/( IOperand const & rhs ) const
 {
-	switch (rhs.getType())
-	{
-	case eOperandType::int8 :
-		return (this->_genericOperand<int8_t>(reinterpret_cast< TOperand<int8_t> const * >(&rhs), operationType::divOp ));
-		break ;
-	case eOperandType::int16 :
-		return (this->_genericOperand<int16_t>( reinterpret_cast< TOperand<int16_t> const * >(&rhs), operationType::divOp ));
-		break ;
-	case eOperandType::int32 :
-		return (this->_genericOperand<int32_t>(  reinterpret_cast< TOperand<int32_t> const * >(&rhs), operationType::divOp ));
-		break ;
-	case eOperandType::floatt :
-		return (this->_genericOperand<float>(  reinterpret_cast< TOperand<float> const * >(&rhs), operationType::divOp ));
-		break ;
-	case eOperandType::doublee :
-		return (this->_genericOperand<double>(  reinterpret_cast< TOperand<double> const * >(&rhs), operationType::divOp ));
-		break ;
-	}
-
-	return this;
+	return (this->_castOperand(rhs, operationType::divOp));
 }
 
 template <typename T>
@@ -452,166 +292,55 @@ IOperand const * TOperand<T>::operator%( IOperand const & rhs ) const
 	// switch (rhs.getType())
 	// {
 	// case eOperandType::int8 :
-	// 	return (this->_genericOperand<int8_t>(reinterpret_cast< TOperand<int8_t> const * >(&rhs), operationType::modOp ));
+	// 	return (this->_genericOperation<int8_t>(reinterpret_cast< TOperand<int8_t> const * >(&rhs), operationType::modOp ));
 	// 	break ;
 	// case eOperandType::int16 :
-	// 	return (this->_genericOperand<int16_t>( reinterpret_cast< TOperand<int16_t> const * >(&rhs), operationType::modOp ));
+	// 	return (this->_genericOperation<int16_t>( reinterpret_cast< TOperand<int16_t> const * >(&rhs), operationType::modOp ));
 	// 	break ;
 	// case eOperandType::int32 :
-	// 	return (this->_genericOperand<int32_t>(  reinterpret_cast< TOperand<int32_t> const * >(&rhs), operationType::modOp ));
+	// 	return (this->_genericOperation<int32_t>(  reinterpret_cast< TOperand<int32_t> const * >(&rhs), operationType::modOp ));
 	// 	break ;
 	// case eOperandType::floatt :
 	// 	std::cout << "ERROR NO MOD ON FLOAT !!" << std::endl;
-	// 	// return (this->_genericOperand<float>(  reinterpret_cast< TOperand<float> const * >(&rhs), operationType::modOp ));
+	// 	// return (this->_genericOperation<float>(  reinterpret_cast< TOperand<float> const * >(&rhs), operationType::modOp ));
 	// 	break ;
 	// case eOperandType::doublee :
 	// 	std::cout << "ERROR NO MOD ON DOUBLE !!" << std::endl;
-	// 	// return (this->_genericOperand<double>(  reinterpret_cast< TOperand<double> const * >(&rhs), operationType::modOp ));
+	// 	// return (this->_genericOperation<double>(  reinterpret_cast< TOperand<double> const * >(&rhs), operationType::modOp ));
 	// 	break ;
 	// }
 	(void)rhs;
 	return this;
 }
-// template <typename T>
-// IOperand const * TOperand<T>::operator+( IOperand const & rhs ) const
-// {
-// 	switch (rhs.getType())
-// 	{
-// 	case eOperandType::int8 :
-// 		return (this->_addOperand<int8_t>(reinterpret_cast< TOperand<int8_t> const * >(&rhs)));
-// 		break ;
-// 	case eOperandType::int16 :
-// 		return (this->_addOperand<int16_t>( reinterpret_cast< TOperand<int16_t> const * >(&rhs)));
-// 		break ;
-// 	case eOperandType::int32 :
-// 		return (this->_addOperand<int32_t>(  reinterpret_cast< TOperand<int32_t> const * >(&rhs)));
-// 		break ;
-// 	case eOperandType::floatt :
-// 		return (this->_addOperand<float>(  reinterpret_cast< TOperand<float> const * >(&rhs)));
-// 		break ;
-// 	case eOperandType::doublee :
-// 		return (this->_addOperand<double>(  reinterpret_cast< TOperand<double> const * >(&rhs)));
-// 		break ;
-// 	}
-
-// 	return this;
-// }
-
-// template <typename T>
-// IOperand const * TOperand<T>::operator-( IOperand const & rhs ) const
-// {
-// 	switch (rhs.getType())
-// 	{
-// 	case eOperandType::int8 :
-// 		return (this->_subOperand<int8_t>(reinterpret_cast< TOperand<int8_t> const * >(&rhs)));
-// 		break ;
-// 	case eOperandType::int16 :
-// 		return (this->_subOperand<int16_t>( reinterpret_cast< TOperand<int16_t> const * >(&rhs)));
-// 		break ;
-// 	case eOperandType::int32 :
-// 		return (this->_subOperand<int32_t>(  reinterpret_cast< TOperand<int32_t> const * >(&rhs)));
-// 		break ;
-// 	case eOperandType::floatt :
-// 		return (this->_subOperand<float>(  reinterpret_cast< TOperand<float> const * >(&rhs)));
-// 		break ;
-// 	case eOperandType::doublee :
-// 		return (this->_subOperand<double>(  reinterpret_cast< TOperand<double> const * >(&rhs)));
-// 		break ;
-// 	}
-
-// 	return this;
-// }
-
-// template <typename T>
-// IOperand const * TOperand<T>::operator*( IOperand const & rhs ) const
-// {
-// 	switch (rhs.getType())
-// 	{
-// 	case eOperandType::int8 :
-// 		return (this->_mulOperand<int8_t>(reinterpret_cast< TOperand<int8_t> const * >(&rhs)));
-// 		break ;
-// 	case eOperandType::int16 :
-// 		return (this->_mulOperand<int16_t>( reinterpret_cast< TOperand<int16_t> const * >(&rhs)));
-// 		break ;
-// 	case eOperandType::int32 :
-// 		return (this->_mulOperand<int32_t>(  reinterpret_cast< TOperand<int32_t> const * >(&rhs)));
-// 		break ;
-// 	case eOperandType::floatt :
-// 		return (this->_mulOperand<float>(  reinterpret_cast< TOperand<float> const * >(&rhs)));
-// 		break ;
-// 	case eOperandType::doublee :
-// 		return (this->_mulOperand<double>(  reinterpret_cast< TOperand<double> const * >(&rhs)));
-// 		break ;
-// 	}
-
-// 	return this;
-// }
-
-// template <typename T>
-// IOperand const * TOperand<T>::operator/( IOperand const & rhs ) const
-// {
-// 	(void)rhs;
-// 	return this;
-// }
-
-// template <typename T>
-// IOperand const * TOperand<T>::operator%( IOperand const & rhs ) const
-// {
-// 	(void)rhs;
-// 	return this;
-// }
 
 template <typename T>
 void	TOperand<T>::_findType( void )
 {
-	// switch (typeid(T))
-	// {
 	if (typeid(T) == typeid(int8_t))
 	{
-	// case typeid(int8_t) :
 		this->_type = eOperandType::int8;
 		this->_typeStr = "int8";
 	}
-	// 	break ;
-	// case typeid(int16_t) :
 	else if (typeid(T) == typeid(int16_t))
 	{
 		this->_type = eOperandType::int16;
 		this->_typeStr = "int16";
 	}
-	// 	break ;
-	// case typeid(int32_t) :
 	else if (typeid(T) == typeid(int32_t))
 	{
 		this->_type = eOperandType::int32;
 		this->_typeStr = "int32";
 	}
-	// 	break ;
-	// case typeid(float) :
 	else if (typeid(T) == typeid(float))
 	{
 		this->_type = eOperandType::floatt;
 		this->_typeStr = "float";
 	}
-	// 	break ;
-	// case typeid(double) :
 	else if (typeid(T) == typeid(double))
 	{
 		this->_type = eOperandType::doublee;
 		this->_typeStr = "double";
 	}
-		// break ;
-	// }
-	// if (typeid(T) == typeid(int8_t))
-	// 	return eOperandType::int8;
-	// else if (typeid(T) == typeid(int16_t))
-	// 	return eOperandType::int16;
-	// else if (typeid(T) == typeid(int32_t))
-	// 	return eOperandType::int32;
-	// else if (typeid(T) == typeid(float))
-	// 	return eOperandType::floatt;
-
-	// return eOperandType::doublee;
 }
 
 #endif // ** TOPERAND_HPP ** //
